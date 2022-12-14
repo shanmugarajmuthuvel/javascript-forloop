07283a2536388e60727b9fc8f7beac1344b9dd16
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2022-12-14 11:51:31 +0530

    for-loop

---
+++ test.js
@@ -0,0 +1,10 @@
+	var a=parseInt(prompt("Enter The Length"))
+var c=0
+ for(i=0; i<a; i++){
+  b=parseInt(prompt("Sum Of PositiveNumbers"))
+   if(0<b){
+       c=c+b
+		 }
+ }	
+    document.write(c)
+	
